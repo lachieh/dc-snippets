43e07f536beaaa871f0412389b240f78a6fa763a
Lachlan Heywood <lachlan@heywood.net.au>
2020-11-28 08:36:22 -0500

    update app controller fw/ new auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,19 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Post('/auth/github/login')
-  @UseGuards(AuthGuard('github'))
-  async login(@Request() req) {
-    return req.user;
-  }
-
-  @Get('/auth/github/callback')
-  @UseGuards(AuthGuard('github'))
-  async loginCallback(@Request() req) {
-    return req.user;
+  getHello(@Req() req): string {
+    return this.appService.getHello(req);
   }
 }
